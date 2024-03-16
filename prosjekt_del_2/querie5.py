c35cee40310922f50139d14aa929134c6b116ee5
Nora Haakenstad <norahaak@stud.ntnu.no>
2024-03-16 12:49:31 +0100

    querie5 add

--- prosjekt_del_2/querie5.py
+++ prosjekt_del_2/querie5.py
@@ -2,3 +2,11 @@ import sqlite3
 con = sqlite3.connect("Theatre.db")
 cursor = con.cursor()
 
+
+query = "SELECT Ansatt.Navn, Teaterstykke.Stykketittel, Rolle.Oppgavenavn \
+         FROM ansatt \
+         JOIN Rolle ON Ansatt.AnsattID = Rolle.AnsattID \
+         JOIN Teaterstykke ON Rolle.TeaterstykkeID = Teaterstykke.TeaterstykkeID"
+cursor.execute(query)
+row = cursor.fetchall()
+print("Navn på skuespillere og roller som opptrer i teaterstykkene: ", row)
