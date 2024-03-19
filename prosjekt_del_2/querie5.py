84de9ed7581d3a9c68957671ff363aeaa058abef
norahawk <norahaak@ntnu.no>
2024-03-19 10:50:54 +0100

    q5

--- prosjekt_del_2/querie5.py
+++ prosjekt_del_2/querie5.py
@@ -5,7 +5,7 @@ cursor = con.cursor()
 
 query = """
     SELECT Ansatt.Fornavn, Ansatt.Etternavn, Teaterstykke.Stykketittel, Rolle.Oppgavenavn 
-    FROM Ansatt 
+    FROM Ansatt
     JOIN HarOppgaver ON Ansatt.AnsattID = HarOppgaver.AnsattID 
     JOIN Rolle ON HarOppgaver.OppgaveID = Rolle.OppgaveID AND HarOppgaver.StykkeID = Rolle.StykkeID 
     JOIN Teaterstykke ON Rolle.StykkeID = Teaterstykke.StykkeID
@@ -13,3 +13,4 @@ query = """
 cursor.execute(query)
 row = cursor.fetchall()
 print("Navn på skuespillere og roller som opptrer i teaterstykkene:", row)
+con.close()
