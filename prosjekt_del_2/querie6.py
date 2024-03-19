3fee605ac74b293bba22c6e3bd121bcd73681286
norahawk <norahaak@ntnu.no>
2024-03-19 10:48:39 +0100

    m

--- prosjekt_del_2/querie6.py
+++ prosjekt_del_2/querie6.py
@@ -1,7 +1,3 @@
-"""Vi ønsker å lage et query i SQL som finner hvilke forestillinger som har solgt
-best. Skriv ut navn på forestilling og dato og antall solgte plasser sortert på
-antall plasser i synkende rekkefølge"""
-
 import sqlite3
 con = sqlite3.connect("Theatre.db")
 cursor = con.cursor()
