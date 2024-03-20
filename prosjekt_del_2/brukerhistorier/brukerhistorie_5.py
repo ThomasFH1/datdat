1c1a84fd98cef7ffc17fe85253dbb73ed0797c5b
Thomas Falkeid Hagland <thomas.hagland03@gmail.com>
2024-03-20 15:49:06 +0100

    brukerhistorie 5 korrigert

--- prosjekt_del_2/brukerhistorier/brukerhistorie_5.py
+++ prosjekt_del_2/brukerhistorier/brukerhistorie_5.py
@@ -8,11 +8,12 @@ class Brukerhistorie5(Brukerhistorie):
             cursor = con.cursor()
 
             query = """
-                SELECT Ansatt.Fornavn, Ansatt.Etternavn, Teaterstykke.Stykketittel, Rolle.Oppgavenavn 
-                FROM Ansatt
-                JOIN HarOppgaver ON Ansatt.AnsattID = HarOppgaver.AnsattID 
-                JOIN Rolle ON HarOppgaver.OppgaveID = Rolle.OppgaveID AND HarOppgaver.StykkeID = Rolle.StykkeID 
-                JOIN Teaterstykke ON Rolle.StykkeID = Teaterstykke.StykkeID
+                SELECT Teaterstykke.Stykketittel, Bruker.Fornavn || ' ' || Bruker.Etternavn AS Fullt_navn, Rolle.Oppgavenavn
+                FROM Bruker
+                JOIN Ansatt ON Ansatt.BrukerID = Bruker.BrukerID
+                JOIN HarOppgaver ON Ansatt.AnsattID = HarOppgaver.AnsattID
+                JOIN Rolle ON HarOppgaver.OppgaveID = Rolle.OppgaveID AND HarOppgaver.StykkeID = Rolle.StykkeID
+                JOIN Teaterstykke ON Rolle.StykkeID = Teaterstykke.StykkeID;
             """
             cursor.execute(query)
             row = cursor.fetchall()
