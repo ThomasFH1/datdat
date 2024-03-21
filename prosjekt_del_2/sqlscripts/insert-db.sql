9847dfe92aefd57d6d7d1d3e9f9dcc1b05f08591
Thomas Falkeid Hagland <thomas.hagland03@gmail.com>
2024-03-21 22:16:20 +0100

    oppgave 3

--- prosjekt_del_2/sqlscripts/insert-db.sql
+++ prosjekt_del_2/sqlscripts/insert-db.sql
@@ -91,10 +91,10 @@ VALUES
 
 INSERT INTO Ansatt (Personnummer, AnsattID)
 VALUES 
-(00000000021, 20),
-(00000000022, 21),
-(00000000023, 22),
-(00000000024, 23);
+('00000000021', 20),
+('00000000022', 21),
+('00000000023', 22),
+('00000000024', 23);
 
 /*Setter inn kunstnerisk lag til SAAEK først som brukere, så ansatte*/
 INSERT INTO Bruker (Fornavn, Etternavn, Telefonnummer, Adresse)
@@ -107,11 +107,11 @@ VALUES
 
 INSERT INTO Ansatt (Personnummer, AnsattID)
 VALUES 
-(00000000025, 24),
-(00000000026, 25),
-(00000000027, 26),
-(00000000028, 27),
-(00000000029, 28);
+('00000000025', 24),
+('00000000026', 25),
+('00000000027', 26),
+('00000000028', 27),
+('00000000029', 28);
 
 /*Fremvisninger SAAEK, pass på at TeaterID og StykkeID er riktig*/
 INSERT INTO Fremvisning (Fremvisningstidspunkt, Salnavn, TeaterID, StykkeID)
@@ -126,7 +126,7 @@ VALUES
 INSERT INTO Fremvisning (Fremvisningstidspunkt, Salnavn, TeaterID, StykkeID)
 VALUES 
 ('2024-03-19 18:30', 'Hovedscenen', 1, 2),
-('2024-04-02 18:30', 'Hovedscenen', 1, 2),
+('2024-02-03', 'Hovedscenen', 1, 2),
 ('2024-04-03 18:30', 'Hovedscenen', 1, 2),
 ('2024-04-11 18:30', 'Hovedscenen', 1, 2),
 ('2024-04-12 18:30', 'Hovedscenen', 1, 2);
